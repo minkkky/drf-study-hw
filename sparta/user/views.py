21dd9aa5bc65d7f14dd2c29a9831246dab0dd984
m_ink <mp98dl4@gmail.com>
2022-06-16 22:14:41 +0900

    10번?

--- sparta/user/views.py
+++ sparta/user/views.py
@@ -1,16 +1,45 @@
-from django.contrib.auth import login, authenticate
+from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework import permissions
+from rest_framework.response import Response
 
-class UserApiView(APIView):
+from django.contrib.auth import login, logout, authenticate
+
+
+class UserView(APIView):
+    permission_classes = [permissions.IsAuthenticated]
+
+    # 사용자 정보 조회
+    def get(self, request):
+        return Response({"message": "get method"})
+
+    # 회원가입
+    def post(self, request):
+        return Response({"message": "post method!!"})
+
+    # 회원 정보 수정
+    def put(self, request):
+        return Response({"message": "put method!!"})
+
+    # 회원 탈퇴
+    def delete(self, request):
+        return Response({"message": "delete method!!"})
+
+
+class UserAPIView(APIView):
     # 로그인
     def post(self, request):
         username = request.data.get('username', '')
         password = request.data.get('password', '')
 
         user = authenticate(request, username=username, password=password)
+
         if not user:
-            return Response({"error": "존재하지 않는 계정이거나 패스워드가 일치하지 않습니다."}, status=status.HTTP_401_UNAUTHORIZED)
+            return Response({"error": "존재하지 않는 계정이거나 패스워드가 일치하지 않습니다."})
 
         login(request, user)
-        return Response({"message": "로그인 성공!!"}, status=status.HTTP_200_OK)
-
-
+        return Response({"message": "login success!!"})
+    
+    def delete(self, request):
+        logout(request)
+        return Response({"message": "logout success!!"})
